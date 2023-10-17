5522cba54095ebc05fe95edbf5e81ff52b66d274
evanmamesah <reinheartmamesah@gmail.com>
2023-10-17 21:23:43 +0800

    first commit

---
+++ exercise8/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./components/home.jsx";
+import Footer from "./components/footer.jsx";
+import Search from "./components/search.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Footer />
+      <Search />  
+    </>
+  );
+};
+
+export default App;
