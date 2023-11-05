a6ef0b699e58f14413da8e190c9b46f09535ae7e
evan <63502524+evanmamesah@users.noreply.github.com>
2023-11-05 23:11:44 +0800

    Update props header.jsx

--- exercise8/src/components/Header/header.jsx
+++ exercise8/src/components/Header/header.jsx
@@ -1,5 +1,6 @@
-const Header = () => {
-  return <h1>Header</h1>;
+/* eslint-disable react/prop-types */
+const Header = ({ title }) => {
+  return <button>{title}</button>;
 };
 
 export default Header;
